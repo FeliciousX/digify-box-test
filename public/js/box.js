356755d3739fcc1f80d79f504f76dee071b01e23
FeliciousX <feliciousx@gmail.com>
2015-01-31 22:30:00 +0800

    So now user know it's loading. Fixes #1

--- public/js/box.js
+++ public/js/box.js
@@ -1,8 +1,12 @@
 function previewFile($id) {
+    // show spinner while image loads
+    $('#loader').show();
+
     $('#previewPhoto').modal('show');
     var $img = $('<img />').attr('src', '/photo/'+$id);
     $img.addClass('img-responsive').addClass('img-thumbnail');
 
+    // once image loads, hide spinner and show image
     $img.load(function() {
         $('#placeholder').append($img);
         $('#loader').hide();
@@ -11,7 +15,7 @@ function previewFile($id) {
 
 $(function(){
     $('#previewPhoto').on('hidden.bs.modal', function(e) {
+        // when modal is hidden, remove image node to prepare for another image node
         $('#placeholder').find('img').first().remove();
-        $('#loader').show();
     });
 });
